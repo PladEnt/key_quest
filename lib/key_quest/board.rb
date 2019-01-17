f28d458c11f5abf074f7071988f0ccc80cb3030e
Joshua D. Forrest <joshuadforrest@gmail.com>
2019-01-17 14:40:46 +0000

    make the exiting

--- lib/key_quest/board.rb
+++ lib/key_quest/board.rb
@@ -1,35 +1,49 @@
 
 class Board
   @@cell
+  @@level
 
   def initialize
     @@cell = [" ", " ", " ", "!", " ", " ", " ", "8", " "]
-
+    @@level = 0
   end
 
   def self.puts_board
     puts " "
-    puts "+---+=0=+---+"
-    puts "| #{@@cell[0]} | #{@@cell[1]} | #{@@cell[2]} |"
-    puts "|---+---+---|"
-    puts "| #{@@cell[3]} | #{@@cell[4]} | #{@@cell[5]} |"
-    puts "|---+---+---|"
+    puts "+---+=0=+---+---------+"
+    puts "| #{@@cell[0]} | #{@@cell[1]} | #{@@cell[2]} | your on |"
+    puts "|---+---+---|  level  |"
+    puts "| #{@@cell[3]} | #{@@cell[4]} | #{@@cell[5]} +---------+"
+    puts "|---+---+---|    #{@@level}"
     puts "| #{@@cell[6]} | #{@@cell[7]} | #{@@cell[8]} |"
-    puts "+---+-=-+---+"
+    puts "+---+-=-+---+---+"
+    Board.update_board
   end
 
   def self.update_board
-    puts "|make you move|"
+    puts "| make your move |"
     puts "+-------------+"
     imput = gets.chomp.strip
 
     if imput == "w"
+      Board.puts_board
 
     elsif imput == "s"
+      Board.puts_board
 
     elsif imput == "a"
+      Board.puts_board
 
     elsif imput == "d"
+      Board.puts_board
+
+    elsif imput == "exit"
+      num = @@level
+      CLI.exit(num)
+      exit
+
+    else
+      Board.puts_board
 
     end
   end
--- lib/key_quest/cli.rb
+++ lib/key_quest/cli.rb
@@ -6,15 +6,21 @@ class CLI
   end
 
   def welcome
+    puts '           () '
+    puts '   _       \/       _ '
+    puts '0_/ \------/\------/ \_0 '
+    puts '   /\/\/\/\/\/\/\/\/\ '
+    puts '  /\/\/\/\/\/\/\/\/\/\ '
+    puts ' /\/\/\/\/\/\/\/\/\/\/\ '
     puts "+----------------------+"
     puts "| WELCOME TO KEY QUEST |"
     puts "+----------------------+"
-    puts "|the goel in thes game |"
+    puts "|The goel in thes game |"
     puts "|is to git the key and |"
     puts "|make in to the door to|"
     puts "|move on, the levels   |"
     puts "|git harder as you go. |"
-    puts "|lets see how fare you |"
+    puts "|Lets see how fare you |"
     puts "|       can go!        |"
     puts "+----------------------+"
     puts "|use 'w/a/s/d' to wove |"
@@ -22,6 +28,23 @@ class CLI
 
     Board.new
     Board.puts_board
-    Board.update_board
+  end
+
+  def self.exit(level)
+    puts '           () '
+    puts '   _       \/       _ '
+    puts '0_/ \------/\------/ \_0 '
+    puts '   /\/\/\/\/\/\/\/\/\ '
+    puts '  /\/\/\/\/\/\/\/\/\/\ '
+    puts ' /\/\/\/\/\/\/\/\/\/\/\ '
+    puts "+----------------------+"
+    puts "|      Thank You!      |"
+    puts "          #{}"
+    puts "+----------------------+"
+    puts "| You made it to level |"
+    puts "           #{level}"
+    puts "+----------------------+"
+    puts "|   Exiting... Done!   |"
+    puts "+----------------------+"
   end
 end
